31d0574bff8cd655f6d63efc7d3c982bbcb81f67
sirio <sirio.carino@eudata.com>
2018-07-23 17:28:55 +0200

    fix

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/webhook', (req, res) => {
 
   let body = req.body;
   console.log(body)
-  console.log(body.nlp.entities)
+  console.log(JSON.stringify(body))
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
