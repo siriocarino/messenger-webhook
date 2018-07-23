4470eebc0bbcbd31c1f2740958c3263e5f47caa6
sirio <sirio.carino@eudata.com>
2018-07-23 14:47:54 +0200

    fix

--- index.js
+++ index.js
@@ -13,7 +13,33 @@ app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/webhook', function(req, res) {
-  res.status(200).send('PAGE');
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "270680"
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+       // Checks the mode and token sent is correct
+       if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+       } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+       }
+  }
+
+
+
 });
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
@@ -40,30 +66,28 @@ app.post('/webhook', (req, res) => {
   }
 
 
+// Your verify token. Should be a random string.
+let VERIFY_TOKEN = "270680"
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "270680"
+// Parse the query params
+let mode = req.query['hub.mode'];
+let token = req.query['hub.verify_token'];
+let challenge = req.query['hub.challenge'];
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+// Checks if a token and mode is in the query string of the request
+if (mode && token) {
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
+     // Checks the mode and token sent is correct
+     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-       // Checks the mode and token sent is correct
-       if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+          // Responds with the challenge token from the request
+          console.log('WEBHOOK_VERIFIED');
+          res.status(200).send(challenge);
 
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-       } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-       }
-  }
-
+     } else {
+          // Responds with '403 Forbidden' if verify tokens do not match
+          res.sendStatus(403);
+     }
+}
 
 });
