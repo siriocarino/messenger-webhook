f9d5d4d9845457285e77d79019041060ca28e02d
sirio <sirio.carino@eudata.com>
2018-07-25 10:53:12 +0200

    i

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ app.post('/webhook', (req, res) => {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
 
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
